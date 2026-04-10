3e071d7cf9217104d7a73fd16a9165f67651d8f5
Oscarro11 <oscarrodrigomelchore@gmail.com>
2026-04-10 15:57:05 -0600

    feat: User can upload excel file for data analysis

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 class app():
     def __init__(self) -> None:
-        file = st.file_uploader("Elegir archivo de notas", type=".csv")
+        self.tables = {}
+
+        st.set_page_config(page_title="Resumen de Tablas", layout="wide")
+        st.title("Analisis de notas de estudiante")
+        file = st.file_uploader("Elegir archivo de notas", type="xlsx")
+
 
         if file is not None:
-            df = pd.read_csv(file)
-            st.write(file)
+            excel = pd.ExcelFile(file)
+            sheetnames = excel.sheet_names
+
+            for sheetname in sheetnames:
+                df = pd.read_excel(excel, sheet_name=sheetname)
+                self.tables[f"{sheetname}"] = df
+
+            selected_table = st.sidebar.selectbox(
+                "Elige la clase para la cual hacer el analisis",
+                list(self.tables.keys())
+            )
+        else:
+            st.write("Elige un archivo de notas para analizar")
+
+app()
