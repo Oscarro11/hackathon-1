a5cd31b4573267bc589a67241fc1a5d9fc80a154
jsndrr <joseandreelopez05@gmail.com>
2026-04-10 17:44:17 -0600

    Update graph_rendering.py

--- graph_rendering.py
+++ graph_rendering.py
@@ -1,13 +1,18 @@
+#Esta función genera un gráfico de barras comparando el aporte real de cada tarea con su porcentaje, usando matplotlib, y luego lo muestra en la interfaz de Streamlit
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
-
+# Función para graficar aportes vs porcentajes
+# Crear posiciones numéricas para cada tarea (0,1,2,3...)
+# Crear figura y eje del gráfico
 def value_vs_weight_graph(tareas, aportes, porcentajes):
     x = np.arange(len(tareas))  # posiciones
     width = 0.35  # ancho de barras
 
     fig, ax = plt.subplots()
-
+# Barras de "aporte real"
+    # ---------- Configuración visual ----------
+#este apartado ayuda en el diseño mostrandonos como titulos nobres esto ayudando a identificar lo que se visualiza
     # Barras
     ax.bar(x - width/2, aportes, width, label="Aporte real")
     ax.bar(x + width/2, porcentajes, width, label="Porcentaje")
